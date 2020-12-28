b1c97ff781172ee83a3bb7e0e0f01c702d972d19
Michael Chang <b02605061@gmail.com>
2020-12-27 23:50:40 -0500

    connected MongoDB

--- backend/server.py
+++ backend/server.py
@@ -1,1 +1,13 @@
+"""This is the backend for T-of-Yinzi"""
 import pymongo
+from pymongo import MongoClient
+import config
+
+
+cluster = MongoClient(config.ATLAS_URI)
+db = cluster["t-of-yinzi"]
+collection = db["test"]
+
+post1 = {"_id": 0, "name": "yinzi"}
+
+collection.insert(post1)
