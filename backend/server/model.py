be9136aff68caf94ad23a110080f2b66dba625f1
Michael Chang <michael@MichaeldeMacBook-Pro.local>
2021-07-04 00:46:45 -0400

    add /users/add rout

--- backend/server/model.py
+++ backend/server/model.py
@@ -1,12 +1,42 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
+from bson import ObjectId
+
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
 
 # Define models
-class User(BaseModel):
-    _id: int
+class UserModel(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     name: str
     isTeacher: bool
     description: Optional[str] = None
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "name": "Yinzi Zhou",
+                "isTeacher": True
+                }
+        }
 
 
 
--- backend/server/users.py
+++ backend/server/users.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
+from .model import UserModel
 import os
 
 
@@ -25,4 +28,17 @@ async def get_all_users_count() -> dict:
     users_count = await db["t-of-yinzi"]["test"].count_documents({})
     db.close()
     return users_count
+
+@router.post("/add", response_description="Add new user", response_model=UserModel)
+async def add_user(user: UserModel = Body(...)):
+    user = jsonable_encoder(user)
+    db = AsyncIOMotorClient(ATLAS_URI)
+    new_user = await db["t-of-yinzi"]["test"].insert_one(user)
+    created_user = await db["t-of-yinzi"]["test"].find_one({"id": new_user.inserted_id})
+    db.close()
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_user)
+
+
+
+
 
