20c15e405ef188cf8174106ae2093152398cd486
Michael Chang <michael@MichaeldeMacBook-Pro.local>
2021-07-09 15:08:42 -0400

    adding player to media

--- frontend/t-of-yinzi/src/components/Media.jsx
+++ frontend/t-of-yinzi/src/components/Media.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import ReactPlayer from 'react-player';
+import './Media.css'
 
 function Media() {
     return (
 
-        <h1>Media</h1>
+        <>
+            <h1>Media</h1>
+            <div className="media-container">
+
+                <ReactPlayer
+                    url="https://www.youtube.com/watch?v=UcgNYJeV_o8"
+                    controls
+                />
+            </div>
+        </>
 
     );
 }
